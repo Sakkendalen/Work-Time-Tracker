0ded2a8a1603eb0f6a48532441c7804185048503
Saku Tynjälä <sakutynjala@gmail.com>
2019-03-20 10:13:50 +0200

    add Layout hours, calendar. add activity hours, calendar.
    -Layout insert hours.
    -Layout calendar.
    -Activity Insert hours.
    -Activity Calendar

---
+++ app/src/main/java/fi/tuni/work_time_tracker/HoursCalendar.java
@@ -0,0 +1,33 @@
+package fi.tuni.work_time_tracker;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class HoursCalendar extends Activity {
+
+    CalendarView simpleCalendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourscalendar);
+        simpleCalendarView = (CalendarView) findViewById(R.id.simpleCalendarView); // get the reference of CalendarView
+        simpleCalendarView.setFocusedMonthDateColor(Color.RED); // set the red color for the dates of  focused month
+        simpleCalendarView.setUnfocusedMonthDateColor(Color.BLUE); // set the yellow color for the dates of an unfocused month
+        simpleCalendarView.setSelectedWeekBackgroundColor(Color.RED); // red color for the selected week's background
+        simpleCalendarView.setWeekSeparatorLineColor(Color.GREEN); // green color for the week separator line
+        // perform setOnDateChangeListener event on CalendarView
+        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // display the selected date by using a toast
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/fi/tuni/work_time_tracker/InsertHoursActivity.java
@@ -0,0 +1,13 @@
+package fi.tuni.work_time_tracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InsertHoursActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserthours);
+    }
+}
